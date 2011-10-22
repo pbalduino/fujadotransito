90db6afc94b0dafd9d9d26475c5023ff8b58bdcf
Plinio Balduino <pbalduino+github@gmail.com>
2011-10-22 19:02:59 -0200

    First day commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fujadotransito::Application.config.secret_token = 'f0fc8da0a00498bab2cf553d7d61ee058f59a99c20f3f75f3fe6ce3337383024cf8c9a76202892b1cd1b935550898fcf4b03ff72398ce1ae7e1425194dd74a2d'
---
+++ public/javascripts/test.js
@@ -0,0 +1,304 @@
+var myPath = {
+   "routes" : [
+      {
+         "bounds" : {
+            "northeast" : {
+               "lat" : -23.581460,
+               "lng" : -46.665060
+            },
+            "southwest" : {
+               "lat" : -23.610220,
+               "lng" : -46.69477000000001
+            }
+         },
+         "copyrights" : "Map data ©2011 MapLink",
+         "legs" : [
+            {
+               "distance" : {
+                  "text" : "5.6 km",
+                  "value" : 5619
+               },
+               "duration" : {
+                  "text" : "13 mins",
+                  "value" : 788
+               },
+               "end_address" : "R. Arizona, 1426 - Itaim Bibi, Sao Paulo - São Paulo, 04567-003, Brazil",
+               "end_location" : {
+                  "lat" : -23.610220,
+                  "lng" : -46.69477000000001
+               },
+               "start_address" : "Av. Brg. Luís Antônio, 4700 - Jardim Paulista, Sao Paulo - São Paulo, 01402-002, Brazil",
+               "start_location" : {
+                  "lat" : -23.58210,
+                  "lng" : -46.66668000000001
+               },
+               "steps" : [
+                  {
+                     "distance" : {
+                        "text" : "0.1 km",
+                        "value" : 133
+                     },
+                     "duration" : {
+                        "text" : "1 min",
+                        "value" : 19
+                     },
+                     "end_location" : {
+                        "lat" : -23.581460,
+                        "lng" : -46.66558000000001
+                     },
+                     "html_instructions" : "Head \u003cb\u003enortheast\u003c/b\u003e on \u003cb\u003eAv. Brg. Luís Antônio\u003c/b\u003e toward \u003cb\u003eR. Vieira Maciel\u003c/b\u003e",
+                     "polyline" : {
+                        "points" : "b{|nCvqy{G_C{E"
+                     },
+                     "start_location" : {
+                        "lat" : -23.58210,
+                        "lng" : -46.66668000000001
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "0.1 km",
+                        "value" : 104
+                     },
+                     "duration" : {
+                        "text" : "1 min",
+                        "value" : 22
+                     },
+                     "end_location" : {
+                        "lat" : -23.582260,
+                        "lng" : -46.665060
+                     },
+                     "html_instructions" : "Take the 1st \u003cb\u003eright\u003c/b\u003e onto \u003cb\u003eR. Vieira Maciel\u003c/b\u003e",
+                     "polyline" : {
+                        "points" : "bw|nCzjy{GbAk@zA{@"
+                     },
+                     "start_location" : {
+                        "lat" : -23.581460,
+                        "lng" : -46.66558000000001
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "0.4 km",
+                        "value" : 376
+                     },
+                     "duration" : {
+                        "text" : "1 min",
+                        "value" : 69
+                     },
+                     "end_location" : {
+                        "lat" : -23.584140,
+                        "lng" : -46.668120
+                     },
+                     "html_instructions" : "Take the 1st \u003cb\u003eright\u003c/b\u003e onto \u003cb\u003eR. Bento de Andrade\u003c/b\u003e",
+                     "polyline" : {
+                        "points" : "b||nCrgy{G|@`BJR`BdDnBnD`ApBXd@"
+                     },
+                     "start_location" : {
+                        "lat" : -23.582260,
+                        "lng" : -46.665060
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "0.1 km",
+                        "value" : 100
+                     },
+                     "duration" : {
+                        "text" : "1 min",
+                        "value" : 20
+                     },
+                     "end_location" : {
+                        "lat" : -23.584980,
+                        "lng" : -46.668480
+                     },
+                     "html_instructions" : "Slight \u003cb\u003eleft\u003c/b\u003e onto \u003cb\u003eR. Cândido do Nascimento\u003c/b\u003e",
+                     "polyline" : {
+                        "points" : "zg}nCvzy{Gz@VjBn@"
+                     },
+                     "start_location" : {
+                        "lat" : -23.584140,
+                        "lng" : -46.668120
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "1.0 km",
+                        "value" : 952
+                     },
+                     "duration" : {
+                        "text" : "2 mins",
+                        "value" : 116
+                     },
+                     "end_location" : {
+                        "lat" : -23.588820,
+                        "lng" : -46.67630
+                     },
+                     "html_instructions" : "Turn \u003cb\u003eright\u003c/b\u003e to merge onto \u003cb\u003eCmpv. Tribunal de Justiça do Estado de São Paulo\u003c/b\u003e",
+                     "polyline" : {
+                        "points" : "bm}nC~|y{GRvA@nF?NCV?DCV?J?L?F?H@L?JLXVr@FNDNd@tBJX`AhDDNFHPXNTDFDH\\j@Zl@v@jAjBfBFFPL^ZdD|Ct@r@Vp@"
+                     },
+                     "start_location" : {
+                        "lat" : -23.584980,
+                        "lng" : -46.668480
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "1.5 km",
+                        "value" : 1454
+                     },
+                     "duration" : {
+                        "text" : "3 mins",
+                        "value" : 155
+                     },
+                     "end_location" : {
+                        "lat" : -23.590290,
+                        "lng" : -46.689910
+                     },
+                     "html_instructions" : "Continue onto \u003cb\u003eAv. Pres. Juscelino Kubitschek\u003c/b\u003e",
+                     "polyline" : {
+                        "points" : "be~nCzm{{GLLNNR\\H\\Jl@XrBr@pDh@|CbAxFV`BPbABPBTl@fDn@pDh@fDXzA\\pAL|@@b@?v@C`@Er@Ot@k@jCUjAKh@WpAYxACNCNq@`EKf@"
+                     },
+                     "start_location" : {
+                        "lat" : -23.588820,
+                        "lng" : -46.67630
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "72 m",
+                        "value" : 72
+                     },
+                     "duration" : {
+                        "text" : "1 min",
+                        "value" : 31
+                     },
+                     "end_location" : {
+                        "lat" : -23.59050,
+                        "lng" : -46.69007000000001
+                     },
+                     "html_instructions" : "Slight \u003cb\u003eleft\u003c/b\u003e toward \u003cb\u003eAv. Pres. Juscelino Kubitschek\u003c/b\u003e",
+                     "polyline" : {
+                        "points" : "hn~nC|b~{GBRCLEN?JDDB@D@D?DGDMLQ"
+                     },
+                     "start_location" : {
+                        "lat" : -23.590290,
+                        "lng" : -46.689910
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "0.1 km",
+                        "value" : 134
+                     },
+                     "duration" : {
+                        "text" : "1 min",
+                        "value" : 37
+                     },
+                     "end_location" : {
+                        "lat" : -23.590840,
+                        "lng" : -46.688810
+                     },
+                     "html_instructions" : "Continue straight onto \u003cb\u003eAv. Pres. Juscelino Kubitschek\u003c/b\u003e",
+                     "polyline" : {
+                        "points" : "ro~nC|c~{GTkAl@oD"
+                     },
+                     "start_location" : {
+                        "lat" : -23.59050,
+                        "lng" : -46.69007000000001
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "0.3 km",
+                        "value" : 272
+                     },
+                     "duration" : {
+                        "text" : "1 min",
+                        "value" : 44
+                     },
+                     "end_location" : {
+                        "lat" : -23.593230,
+                        "lng" : -46.68929000000001
+                     },
+                     "html_instructions" : "Turn \u003cb\u003eright\u003c/b\u003e onto \u003cb\u003eAv. Chedid Jafet\u003c/b\u003e",
+                     "polyline" : {
+                        "points" : "vq~nC`|}{Gt@Pv@RdAX~@RdA?~@?r@DN?^D"
+                     },
+                     "start_location" : {
+                        "lat" : -23.590840,
+                        "lng" : -46.688810
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "0.4 km",
+                        "value" : 447
+                     },
+                     "duration" : {
+                        "text" : "2 mins",
+                        "value" : 102
+                     },
+                     "end_location" : {
+                        "lat" : -23.596750,
+                        "lng" : -46.69128000000001
+                     },
+                     "html_instructions" : "Slight \u003cb\u003eright\u003c/b\u003e onto \u003cb\u003eR. Funchal\u003c/b\u003e",
+                     "polyline" : {
+                        "points" : "t`_oC`_~{G^Jj@Zt@d@NJHFxAdAv@f@xAh@~@Z`@Lh@Jf@FRFf@VLTHFDBFBB@JFJBD@"
+                     },
+                     "start_location" : {
+                        "lat" : -23.593230,
+                        "lng" : -46.68929000000001
+                     },
+                     "travel_mode" : "DRIVING"
+                  },
+                  {
+                     "distance" : {
+                        "text" : "1.6 km",
+                        "value" : 1575
+                     },
+                     "duration" : {
+                        "text" : "3 mins",
+                        "value" : 173
+                     },
+                     "end_location" : {
+                        "lat" : -23.610220,
+                        "lng" : -46.69477000000001
+                     },
+                     "html_instructions" : "Continue onto \u003cb\u003eAv. Eng. Luís Carlos Berrini\u003c/b\u003e\u003cdiv style=\"font-size:0.9em\"\u003eDestination will be on the right\u003c/div\u003e",
+                     "polyline" : {
+                        "points" : "tv_oCnk~{GPFNHNJ`@Lj@Fp@?|@Qv@Ut@QbBe@lBSr@Fp@NxA^h@PdAVtAZhBf@rA`@\\H`Cj@vBh@lD~@zBn@THr@NlCt@fBd@hBb@bCn@THdDv@fBf@zA^DBZH"
+                     },
+                     "start_location" : {
+                        "lat" : -23.596750,
+                        "lng" : -46.69128000000001
+                     },
+                     "travel_mode" : "DRIVING"
+                  }
+               ],
+               "via_waypoint" : []
+            }
+         ],
+         "overview_polyline" : {
+            "points" : "b{|nCvqy{G_C{EbAk@zA{@|@`BlBxDnBnD`ApBXd@z@VjBn@RvA@~FC\\Cx@@V?JLX^bAj@dClAbELXl@`Ax@xAv@jAjBfBXTdExDt@r@Vp@\\\\R\\H\\d@`D|AnIzAzIfArGxAxIXzA\\pAL|@@b@CxAEr@Ot@aAvEaAdFu@pEKf@BRI\\?JDDHBD?DGR_@bA{FrFrAdC?bAD~@P`B`AjD`CxCdAjAXz@Nf@VLTNJJDn@T^T`@Lj@Fp@?|@QlBg@bBe@lBSr@Fp@NbCp@zCr@|DhA~Ct@dHhBpCx@`EdApEhAxCx@lG~A|Bl@"
+         },
+         "summary" : "Av. Pres. Juscelino Kubitschek and Av. Eng. Luís Carlos Berrini",
+         "warnings" : [],
+         "waypoint_order" : []
+      }
+   ],
+   "status" : "OK"
+};
+console.log(myPath);
+
